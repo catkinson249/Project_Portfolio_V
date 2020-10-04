6eb2e068d1d3b2742de7bff83258d2bc9f19a431
Courtney Atkinson <catkinson@AppleCake.local>
2020-10-04 14:26:57 -0500

    Updates

---
+++ precache-manifest.eaa92196df61a5ecdda4189391936582.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf3b5d68bf075bcdffe7b6142ea2aa2",
+    "url": "/fusion_bar-app/index.html"
+  },
+  {
+    "revision": "a315b6ea1bb29dc1501f",
+    "url": "/fusion_bar-app/static/css/main.bccead90.chunk.css"
+  },
+  {
+    "revision": "e80d92263d7f9a88fde3",
+    "url": "/fusion_bar-app/static/js/2.31e5bee9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fusion_bar-app/static/js/2.31e5bee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a315b6ea1bb29dc1501f",
+    "url": "/fusion_bar-app/static/js/main.c0ccce85.chunk.js"
+  },
+  {
+    "revision": "d432987b8c348455ce77",
+    "url": "/fusion_bar-app/static/js/runtime-main.12aae4ef.js"
+  },
+  {
+    "revision": "4623cde46597d9d0f7c02c41e640b543",
+    "url": "/fusion_bar-app/static/media/landing.4623cde4.png"
+  }
+]);
